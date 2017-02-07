bd728db4c8d75e91a86e386340126cac068cdab6
grga911 <grgicnemanja@gmail.com>
2017-02-07 03:20:12 +0100

    Logic change

--- flm.sh
+++ flm.sh
@@ -9,8 +9,8 @@ if [ $# -lt 1 -o "$1" == "--help" -o "$1" == "-h" ]; then
 \t-s\tsource directory (default is current directory)
 \t-t\ttarget directory (default is current directory)
 \t-p\tword default pattern to search for
-\t-e\tfile extension (use -pe when searching only extensions)
-\t-r\tcustom user pattern input (use regex)
+\t-e\tfile extension
+\t-r\tuser custom pattern input
 \t-c\tcopy files instead of moving\n"
  	exit 1
 fi
@@ -40,6 +40,24 @@ function dialog {
 		esac
 }
 
+# Create directory if with given name
+function make_dir {
+
+	directory=$1
+
+	until [[ -d "$directory" ]]; do
+ 		dialog "Directory doesn't exist, do you wish to create it? Y/n"
+		# Defaul choice is yes, so continue if enter is pressed
+ 		if [[ -z "$CHOICE" ]] || [[ "$CHOICE" == "y" ]] && [[ ! -d "$directory" ]] ; then
+ 			mkdir "$directory"
+ 		elif [[ -d "$directory" ]]; then
+ 			continue
+ 		else
+ 			printf "Nothing done exiting \n"
+ 			exit 1
+ 		fi
+done
+}
 #
 #read switches and arguments and for each -switch assing value of it's variable
 #
@@ -47,7 +65,7 @@ function dialog {
 while getopts ":p:r:s:t:e:c" opt; do
  		case $opt in
  		p)
- 			default_pattern=$OPTARG	>&2
+ 			default_pattern=.*$OPTARG.*	>&2
  			;;
  		r)
  			custom_pattern=$OPTARG	>&2
@@ -59,7 +77,7 @@ while getopts ":p:r:s:t:e:c" opt; do
  			target_directory=$OPTARG	>&2
  			;;
  		e)
- 			extension=$OPTARG >&2
+ 			extension=.*$OPTARG'$' >&2
  			;;
  		c)
  			action="cp -vnu"
@@ -91,44 +109,55 @@ done
 #
 #-r switch gives user ability to define his own custom pattern or default pattern
 
+default_pattern=${default_pattern:-"$extension"}
+
 source_dir=${source_dir:-"./"}
 
 action=${action:-"mv -vnu"}
+# Setting name for target directory
+# If no name is provided take pattern for name
+# and replace spaces with _
+# also strip down illegal characters
+if [[ "$target_directory" == "" ]]; then
+	target_folder=$(echo $default_pattern |sed -e 's/ /_/g' -e 's/[!@#$%^&*+=?><,.\/:|`]//g')
+	echo $target_folder
+else
+	target_directory=$(echo $target_directory |sed -e 's/ /_/g' -e 's/[!@#$%^&*+=?><,.\/:|`]//g')
+fi
 
-target_folder="$default_pattern"
 
 target_directory=${target_directory:-"$CURENTDIR/$target_folder"}
 #
-# if there is no -r swith create default default_pattern for search
-# use argument of -p switch to create default custom_pattern default_pattern
+# if there is no -r swith create default pattern for search
+# or use argument of -p switch to create default pattern
 #
-custom_pattern=${custom_pattern:-'.*'"$default_pattern"'.*'"$extension"'$'}
+custom_pattern=${custom_pattern:-"$default_pattern"}
 
-# check if default_pattern is found and if nothing is found ask for new default_pattern
+# check if pattern is found and if nothing is found ask for new pattern
 # exit if user inputs q or Q
 #
-
-until [[ "$result" != "" ]]; do
-
-	result=$(find "$source_dir" -maxdepth 1 -type f -iregex "$custom_pattern")
-	if [[ "$result" == "" ]]; then
+while true; do
+	find "$source_dir" -maxdepth 1 -type f -iregex "$custom_pattern" -printf "%f \n"
+	if [[ $? -ne 0 ]]; then
 		echo "Nothing found, please enter a new pattern, or insert q to quit"
 		read default_pattern
 			if [[ "$default_pattern" == [qQ] ]]; then
 				exit 1
 			fi
 		custom_pattern='.*'"$default_pattern"'.*'"$extension"'$'
+	else
+		break
 	fi
 done
 
 #
-# prints found files so user can verify
+# Prints found files so user can verify
 #
 
-printf "$result\n"
+# printf "$result\n"
 
 #
-# prints were files will be copied or moved
+# Prints were files will be copied or moved
 #
 
 if [[ "$action" == "cp -vnu" ]]; then
@@ -138,12 +167,13 @@ else
 fi
 
 #
-# ask user for confirmation, and if affirmative
-# continues with operation
+# Ask user for confirmation
 #
 until [[ "$CHOICE" = "y" ]]; do
 	printf "\nAre you sure you wish to $default_action files? Y/n \n"
 	dialog
+	# Default choice is yes
+	# If enter is pressed script will continue
 	if [[ -z "$CHOICE" ]]; then
 		CHOICE='y'
 	elif [[ "$CHOICE" != "y" ]]; then
@@ -153,22 +183,12 @@ until [[ "$CHOICE" = "y" ]]; do
 done
 
 #
-#we check if our destination si valid directory, if not we attempt to create it
+# We check if our destination si valid directory, if not we attempt to create it
 #
 
-until [[ -d "$target_directory" ]]; do
- 		dialog "Directory doesn't exist, do you wish to create it? Y/n"
-		
- 		if [[ -z "$CHOICE" ]] || [[ "$CHOICE" == "y" ]] && [[ ! -d "$target_directory" ]] ; then
- 			mkdir "$target_directory"
- 		elif [[ -d "$target_directory" ]]; then
- 			continue
- 		else
- 			printf "Nothing done exiting \n"
- 			exit 1
- 		fi
-done
+make_dir $target_directory
+
 
 #finaly we move or copy our files to a new destination
-		find "$source_dir" -maxdepth 1 -type f -iregex "$custom_pattern" -exec $action {} "$target_directory" \;
+find "$source_dir" -maxdepth 1 -type f -iregex "$custom_pattern" -execdir $action {} "$target_directory" \;
 exit 0
